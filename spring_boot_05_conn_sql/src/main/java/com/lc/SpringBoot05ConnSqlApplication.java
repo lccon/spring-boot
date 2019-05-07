8a34b9bed57e993bea9da606dcd1ec0c989bb6dd
lucong <1193429990@qq.com>
2019-05-07 16:31:53 +0800

    springboot连接数据库使用的jar包

---
+++ spring_boot_05_conn_sql/src/main/java/com/lc/SpringBoot05ConnSqlApplication.java
@@ -0,0 +1,13 @@
+package com.lc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot05ConnSqlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot05ConnSqlApplication.class, args);
+	}
+
+}
